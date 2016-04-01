81bfd6d3ded38bc961b47b4172446ddd4bedee64
Carlos Interaminense <carlos.interaminense@ccc.ufcg.edu.br>
2016-03-31 23:57:12 -0300

    Add classes User, driver and passenger, but aren't finalized

---
+++ my-application-play/app/Models/Driver.java
@@ -0,0 +1,11 @@
+package controllers.Models;
+
+/**
+ * Created by carlos on 27/03/16.
+ */
+public class Driver extends User {
+    public Driver(String registration, String email, String password) throws Exception
+    {
+        super(registration, email, password);
+    }
+}
---
+++ my-application-play/app/Models/Passenger.java
@@ -0,0 +1,12 @@
+package controllers.Models;
+
+/**
+ * Created by carlos on 27/03/16.
+ */
+public class Passenger extends User {
+
+
+    public Passenger(String registration, String email, String password) throws Exception {
+        super(registration, email, password);
+    }
+}
