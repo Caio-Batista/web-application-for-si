54ee2aa4cfe49316b4824c1fde2a1f46f316650b
Caio Oliveira <caio.batista1994@gmail.com>
2016-04-22 00:36:03 -0300

    Missing time recover from carona and link to the driver profile

--- app/DBManager/DBManager.java
+++ app/DBManager/DBManager.java
@@ -1,6 +1,7 @@
 package DBManager;
 
 import controllers.Models.User;
+import controllers.Models.Carona;
 
 import java.util.ArrayList;
 
@@ -17,16 +18,20 @@ public class DBManager {
     {
         User u1 = null;
         User u2 = null;
+        Carona c1 = null;
 
         try {
             u1 = new User("Carlos", "113110864","carlos.interaminense@ccc.ufcg.edu.br", "123456", true, "Aqui", "rua2");
             u2 = new User("Izabella", "113111426","izabella.queiroz@ccc.ufcg.edu.br", "123456", false, "Aqui", "rua2");
+            c1 = new Carona("Aqui", "rua2", "Aqui", "rua2","8:00", 1);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        u1.addCarona(c1);
         db.add(u1);
         db.add(u2);
+
     }
 
    private int returnIndexUser(String email) //aqui
@@ -93,12 +98,35 @@ public class DBManager {
         return true;
     }
 
-    public ArrayList<User> driversPassingByUser(User user){
+    public ArrayList<User> driversPassingByUserStart(User user){
         ArrayList<User> ar = new ArrayList<User>();
         for (User element : db) {
 
-            if (element.getMyCaronasCreateds().getStartingAddress().equals(user.getRoad()))
-                ar.add(element);
+            if (element.isDriver() && element.getMyCaronasCreateds() != null){
+
+            for (Carona car : element.getMyCaronasCreateds()){
+                if (car.getStartingRoad().equals(user.getRoad())){
+
+                    ar.add(element);
+            }
+            }
+        }
+        }
+
+        return ar;
+    }
+
+    public ArrayList<User> driversPassingByUserArrival(User user){
+        ArrayList<User> ar = new ArrayList<User>();
+        for (User element : db) {
+
+            if (element.isDriver() && element.getMyCaronasCreateds() != null){
+
+            for (Carona car: element.getMyCaronasCreateds())
+                 if (car.getArrivalRoad().equals(user.getRoad())){
+                 ar.add(element);
+            }
+        }
         }
 
         return ar;
